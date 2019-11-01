6c138d2102fce18eaa5fdf19e168acd46462cc46
andreia <andreia@andreias-mbp.attlocal.net>
2019-11-01 10:57:34 -0400

    :changing navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Bookster
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/search"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Search
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/books"
+              className={window.location.pathname === "/books" ? "nav-link active" : "nav-link"}
+            >
+              Search
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/search"
+              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+            >
+              Search
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
